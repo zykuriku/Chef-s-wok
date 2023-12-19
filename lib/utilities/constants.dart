cb881fa9c07edf85b2de0923021ae9ea98b5edd0
zykuriku <104116273+zykuriku@users.noreply.github.com>
2023-12-19 23:27:06 +0530

    commit 9

--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -4,6 +4,6 @@ const bgColor = Color(0xffECCA9f);
 const gColor = Color(0xffF3D7C0);
 const nColor = Color(0xffF6BF9A);
 const kColor = Color(0xffBB906B);
-const apiKey = '38e939ca05cd43ab924c68736d6d9fce';
+const apiKey = 'aa8bde61dbf24dfb9be3e1c66b8d4a6d';
 const tStyle = TextStyle(fontSize: 30.0, fontWeight: FontWeight.w600);
 const subStyle = TextStyle(fontSize: 20.0, fontWeight: FontWeight.w400);
